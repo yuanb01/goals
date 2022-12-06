95b0dd2ebb3b147d5592d739a0e3359f63d63796
yuanb01 <yuanb@seas.upenn.edu>
2022-12-05 22:50:15 -0500

    starter code for CLI commands

---
+++ cmd/root.go
@@ -0,0 +1,27 @@
+// package cmd
+
+// import (
+// 	"fmt"
+// 	"os"
+
+// 	"github.com/spf13/cobra"
+// 	"github.com/spf13/viper"
+// )
+
+// var RootCmd = &cobra.Command{
+// 	Use:   "goals",
+// 	Short: "Goals is a goals tracker right in your terminal",
+// 	Long: `A Fast and Flexible Static Site Generator built with
+// 				  love by spf13 and friends in Go.
+// 				  Complete documentation is available at https://gohugo.io/documentation/`,
+// 	Run: func(cmd *cobra.Command, args []string) {
+// 	  // Do Stuff Here
+// 	},
+//   }
+  
+//   func Execute() {
+// 	if err := rootCmd.Execute(); err != nil {
+// 	  fmt.Fprintln(os.Stderr, err)
+// 	  os.Exit(1)
+// 	}
+//   }
